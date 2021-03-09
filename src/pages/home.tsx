758e2fcf8390e30991ecbefe8a21fa9820c79fbc
Túlio Filho <112065@pmenos.com.br>
2021-03-09 17:21:47 -0300

    Criando componentes parte II

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,22 +1,18 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 
-import { format } from 'date-fns'
+import { ArrowRepeat } from 'react-bootstrap-icons';
 import { Container, Row, Col } from 'react-bootstrap'
-import { Wind, Sun, CloudRainHeavyFill, ArrowRepeat } from 'react-bootstrap-icons';
 
-import { RootState } from '../store'
 import { fetchWeather } from '../slices/weather/weather.thunks'
-import { WeatherDay } from '../slices/weather/weather.interfaces'
 
 import WetherAlert from './weather-alert'
 import WetherReport from './weather-report'
+import WetherNextDays from './weather-next-days'
 
 const Home: React.FC = () => {
 
     const dispatch = useDispatch()
-
-    const { weather, current, alerts, weatherDays } = useSelector((application: RootState) => application.weather)
 
     useEffect(() => {
         refreshWeatherReport();
@@ -48,60 +44,13 @@ const Home: React.FC = () => {
         )
     }
 
-    const getDate = (dt: Number): string => {
-        const dtFull = dt as number * 1000;
-
-        return format(new Date(dtFull), 'dd/MM');
-    }
-
-    const renderNextDays = () => {
-        return (
-            <>
-                <Row className={'text-center'} style={{ marginTop: '25px', color: 'white' }}>
-                    <Col md={1}></Col>
-                    <Col md={10}>
-                        <p style={{ fontWeight: 'bold', fontSize: '1.5em' }}>PRÓXIMOS 5 DIAS</p>
-                        <hr style={{ backgroundColor: 'white' }}></hr>
-                    </Col>
-                </Row>
-
-                <Row className={'text-center'} >
-                    <Col md={1}></Col>
-                    {weatherDays.map((item: WeatherDay, index: Number) => {
-                        if (index > 0 && index < 6)
-                            return (
-                                <Col md={2} className={'bkCard'} style={{ color: 'white', fontWeight: 'bold' }} key={item.dt.toString()}>
-                                    <Row>
-                                        <Col md={12} style={{ minHeight: '50px' }}>
-                                            {getDate(item.dt)}
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col md={12} style={{ minHeight: '80px' }}>
-                                            {!item.rain && <Sun size={80} /> || <CloudRainHeavyFill size={80} />}
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col md={12} style={{ minHeight: '50px', marginTop: '25px' }}>
-                                            <p style={{ fontWeight: 'bold', fontSize: '1.5em' }}>{item.temp.day}ºC</p>
-                                        </Col>
-                                    </Row>
-                                </Col>
-                            )
-                    })
-                    }
-                </Row>
-            </>
-        )
-    }
-
     return (
         <Container>
             {renderTitle()}
             <WetherReport />
             <WetherAlert />
-            {renderNextDays()}
-        </Container >
+            <WetherNextDays />
+        </Container>
     )
 }
 
---
+++ src/pages/weather-next-days.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+import { Row, Col } from 'react-bootstrap'
+import { Sun, CloudRainHeavyFill } from 'react-bootstrap-icons';
+
+import { format } from 'date-fns'
+
+import { RootState } from '../store'
+import { WeatherDay } from '../slices/weather/weather.interfaces'
+
+const WetherNextDays: React.FC = () => {
+
+    const { weatherDays } = useSelector((application: RootState) => application.weather)
+
+    const getDate = (dt: Number): string => {
+        const dtFull = dt as number * 1000;
+
+        return format(new Date(dtFull), 'dd/MM');
+    }
+
+    return (
+        <>
+            <Row className={'text-center'} style={{ marginTop: '25px', color: 'white' }}>
+                <Col md={1}></Col>
+                <Col md={10}>
+                    <p style={{ fontWeight: 'bold', fontSize: '1.5em' }}>PRÓXIMOS 5 DIAS</p>
+                    <hr style={{ backgroundColor: 'white' }}></hr>
+                </Col>
+            </Row>
+
+            <Row className={'text-center'} >
+                <Col md={1}></Col>
+                {weatherDays.map((item: WeatherDay, index: Number) => {
+                    if (index > 0 && index < 6)
+                        return (
+                            <Col md={2} className={'bkCard'} style={{ color: 'white', fontWeight: 'bold' }} key={item.dt.toString()}>
+                                <Row>
+                                    <Col md={12} style={{ minHeight: '50px' }}>
+                                        {getDate(item.dt)}
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col md={12} style={{ minHeight: '80px' }}>
+                                        {!item.rain && <Sun size={80} /> || <CloudRainHeavyFill size={80} />}
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col md={12} style={{ minHeight: '50px', marginTop: '25px' }}>
+                                        <p style={{ fontWeight: 'bold', fontSize: '1.5em' }}>{item.temp.day}ºC</p>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        )
+                })
+                }
+            </Row>
+        </>
+    )
+}
+
+export default WetherNextDays
